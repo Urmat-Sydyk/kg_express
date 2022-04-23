b892f7b0cf7f910a164fe554bb81312f19997208
Urmat <sokuluk2707@gmail.com>
2022-04-23 21:45:34 +0600

    add apps and mod

---
+++ backend/apps/product/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField("Название", max_length=50, unique= True)
+    slug = models.SlugField("Слаг", max_length=60, unique= True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+class SubCategory(models.Model):
+    category = models.ForeignKey(Category, on_delete = models.CASCADE, related_name = "subcategory")
+    name = models.CharField("Название", max_length=70, unique=True)
+    slug = models.SlugField("Слаг", max_length=80, unique=True)
+
+    class Meta:
+        verbose_name = 'Подкатегория'
+        verbose_name_plural = 'Подкатегории'
+        ordering = ['category', 'name']
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField("Название", max_length = 255)
+    description = models.TextField("Описание")
+    price = models.DecimalField("Цена", max_digits=10, decimal_places=2)
+    image = models.ImageField("Фото", upload_to= 'product_images/')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name="products")
+    subcategory = models.ForeignKey(SubCategory, on_delete=models.PROTECT, related_name="products")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField("Активный", default=True)
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.name
